7f3fdf8f8a724422a378df26856ee144038d240b
Leandro Amaral <amaral_adm@hotmail.com>
2022-07-20 14:36:35 -0300

    Create Mod-5

---
+++ Mod-05.java
@@ -0,0 +1,25 @@
+/* O objetivo desta tarefa é particar o uso das ferramentas Git e Github */
+
+
+package br.com.amaral;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		String ambiente = args[0];
+
+		if (ambiente.equals("1")) {
+
+			System.out.print("HelloWorld Leandro Amaral");
+
+			} 		
+
+		else {
+
+			System.out.print("HelloWorld - Erro no teste " + args[0]);
+
+			}
+
+		}
+
+	}
