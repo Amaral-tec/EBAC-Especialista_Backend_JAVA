7f88cffda78b30849921f37cef4d18e4db2ba79b
Leandro Amaral <amaral_adm@hotmail.com>
2022-08-13 15:42:59 -0300

    Create Mod-11

---
+++ AppCadastroCliente/src/br/com/appcadastrocliente/Pessoa.java
@@ -0,0 +1,78 @@
+package br.com.appcadastrocliente;
+
+public abstract class Pessoa {
+        private String nome;
+        private String telefone;
+        private String email;
+        private String endereco;
+        private Integer numero;
+        private String bairro;
+        private String cidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    private String estado;
+
+}
